4c6e97226ceafecb270b6beae5623e0191f4b2e1
Nikhil Shahi <shniks@gmail.com>
2022-07-22 11:10:25 -0700

    Created test to check that pantry initially has zero stock.

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -8,11 +8,15 @@ class PantryTest < Minitest::Test
 
   def test_if_it_exists
     pantry = Pantry.new
-
   end
 
+  def test_it_initially_has_zero_stock
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
 
-
-
+    assert_equal ({}), pantry.stock
+    assert_equal 0, pantry.stock_check(ingredient1)
+  end
 
 end
