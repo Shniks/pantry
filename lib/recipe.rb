da876b9eff1ac14c7910a6b5f9a90d40812d2aad
Nikhil Shahi <shniks@gmail.com>
2022-07-22 11:47:45 -0700

    Created method to add the ingredient to the recipe.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,14 +1,17 @@
 class Recipe
 
-  attr_reader :name,  :ingredients_required
+  attr_reader :name,  :ingredients_required,  :ingredients
 
   def initialize(name)
     @name = name
     @ingredients_required = Hash.new(0)
+    @ingredients = []
   end
 
-
-
+  def add_ingredient(ingredient, quantity)
+    ingredients_required[ingredient] += quantity
+    ingredients << ingredient if !ingredients.include?(ingredient)
+  end
 
 
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -19,7 +19,7 @@ class RecipeTest < Minitest::Test
     assert_equal ({}), recipe1.ingredients_required
   end
 
-  def it_can_add_ingredients
+  def test_it_can_add_ingredients
     ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     recipe1 = Recipe.new("Mac and Cheese")
@@ -30,8 +30,8 @@ class RecipeTest < Minitest::Test
 
     expectation = ({ingredient1 => 6, ingredient2 => 8})
 
-    assert_equal expection, recipe1.ingredients_required
-    assert_equal [ingredient1, ingredient2], recipe1.ingredients 
+    assert_equal expectation, recipe1.ingredients_required
+    assert_equal [ingredient1, ingredient2], recipe1.ingredients
   end
 
 end
