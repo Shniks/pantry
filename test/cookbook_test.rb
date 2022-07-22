cd4bb11ce44a42b4a9e575bed4debdd3d0c59e89
Nikhil Shahi <shniks@gmail.com>
2022-07-22 12:24:39 -0700

    Created test to add recipes to cookbook.

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -11,5 +11,16 @@ class CookbookTest < Minitest::Test
 
     assert_instance_of Cookbook, cookbook
   end
-  
+
+  def test_if_it_can_add_recipes_to_cookbook
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    cookbook = Cookbook.new
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal [recipe1, recipe2], cookbook.recipes 
+  end
+
 end
