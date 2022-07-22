e50585629002353c9eb439cf891054eaf0db4665
Nikhil Shahi <shniks@gmail.com>
2022-07-22 10:52:44 -0700

    Created test to check for existence.

---
+++ test/ingredient_test.rb
@@ -0,0 +1,15 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class IngredientTest < Minitest::Test
+
+  def test_if_it_exists
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+
+    assert_instance_of Ingredient, ingredient1
+  end
+
+end
