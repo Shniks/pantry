9998ff4aeb9b1e3a3c1ae37695a7062f8d980069
Nikhil Shahi <shniks@gmail.com>
2022-07-22 11:34:12 -0700

    Created test and method to check for attributes.

---
+++ lib/recipe.rb
@@ -0,0 +1,13 @@
+class Recipe
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+
+
+
+
+end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -12,4 +12,10 @@ class RecipeTest < Minitest::Test
     assert_instance_of Recipe, recipe1
   end
 
+  def test_if_it_has_attributes
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    assert_equal "Mac and Cheese", recipe1.name 
+  end
+
 end
