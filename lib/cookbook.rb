49fe636a18f8600bcda3112d032dc33d80cfdf7e
Nikhil Shahi <shniks@gmail.com>
2022-07-25 11:19:13 -0700

    Created summary method in cookbook class.

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -29,4 +29,20 @@ class Cookbook
     recipes.max_by { |recipe| recipe.total_calories }
   end
 
+  def summary
+    cb_summary = []
+    recipes.each do |recipe|
+      ordered = []
+      order = recipe.ingredients_required.sort_by do |ingredient, quantity|
+        -ingredient.calories * quantity
+      end
+      order.each do |i|
+        ordered << {:ingredient => i.first.name, :amount => "#{i.last} #{i.first.unit}"}
+      end
+      cb_summary << {:name => recipe.name, :details => {:ingredients => ordered, :total_calories =>recipe.total_calories}}
+    end
+    cb_summary
+
+  end
+
 end
--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -46,7 +46,7 @@ class CookbookTest < Minitest::Test
   end
 
   def test_it_can_create_a_cookbook_summary
-    cookbook = CookBook.new
+    cookbook = Cookbook.new
     ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     recipe1 = Recipe.new("Mac and Cheese")
@@ -62,7 +62,7 @@ class CookbookTest < Minitest::Test
     expectation = [{:name=>"Mac and Cheese", :details=>{:ingredients=>[{:ingredient=>"Macaroni", :amount=>"8 oz"}, {:ingredient=>"Cheese", :amount=>"2 C"}], :total_calories=>440}},
                    {:name=>"Burger", :details=>{:ingredients=>[{:ingredient=>"Ground Beef", :amount=>"4 oz"}, {:ingredient=>"Bun", :amount=>"100 g"}], :total_calories=>500}}]
 
-    assert_equal expectation, cookbook.summary 
+    assert_equal expectation, cookbook.summary
   end
 
 end
